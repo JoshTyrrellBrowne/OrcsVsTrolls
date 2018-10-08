4ef4ad41608c8cd11d0a78e9f99d841893a3dbb2
JoshTyrrellBrowne <C00224660@itcarlow.ie>
2018-10-08 17:38:49 +0100

    attack & defend functions added

--- OrcsVsTrolls/OrcsVsTrolls/main.cpp
+++ OrcsVsTrolls/OrcsVsTrolls/main.cpp
@@ -4,11 +4,14 @@
 
 
 std::string getSide(int t_side);          // returns the side which the player is playing with
+void attacking(Orc t_orc, Troll t_troll, int t_meleeOrSpell);
+void gettingAttacked(Orc t_orc, Troll t_troll, int t_meleeOrSpell);
+
 
 int main(void)
 {
 	int sideSelection = 0;
-	int meleeSpell;
+	int meleeOrSpell = 0;
 
 	Orc orcOne(std::string ("Orc Josh"));
 	Orc orcTwo(std::string("Orc Steve"));
@@ -42,42 +45,17 @@ int main(void)
 	std::cout << "\n\n========================================================================================================================" << std::endl;
 
 	std::cout << "Your gang of " + getSide(sideSelection) + " are storming the village." << std::endl;
-	std::cout << orcOne.getName() + " is attacking " + trollOne.getName() + "! \nShould he 1: Melee Attack or 2: Spell Attack ?" << std::endl;
-	std::cin >> meleeSpell;
+	
+	attacking(orcOne, trollOne, meleeOrSpell);
+	attacking(orcTwo, trollTwo, meleeOrSpell);
+	attacking(orcThree, trollThree, meleeOrSpell);
 
-	if (meleeSpell == 1)
-	{
-		if (orcOne.getMeleeNum() > 0)
-		{
-			trollOne.meleeDamage();
-			trollOne.meleeDamageOutput();
-			orcOne.meleeUsed();
-		}
-		else
-		{
-			std::cout << orcOne.getName() + " does not have enough strength left for a melee attack and so he recieves a melee attack." << std::endl;
-			orcOne.meleeDamage();
-			orcOne.meleeDamageOutput();
-		}
-	}
 
-	if (meleeSpell == 2)
-	{
-		if (orcOne.getSpellsNum() > 0)
-		{
-			trollOne.spellDamage();
-			trollOne.spellDamageOutput();
-			orcOne.spellUsed();
-		}
-		else
-		{
-			std::cout << orcOne.getName() + " does not have enough magic left for a spell attack and so he recieves a spell attack." << std::endl;
-			orcOne.spellDamage();
-		}
-	}
 
 	system("pause");
 }
+
+
 
 
 
@@ -95,4 +73,78 @@ std::string getSide(int t_side)
 	}
 
 	return sideString;
+}
+
+void attacking(Orc t_orc, Troll t_troll, int t_meleeOrSpell)
+{
+	std::cout << t_orc.getName() + " is attacking " + t_troll.getName() + "! \nShould he 1: Melee Attack or 2: Spell Attack ?" << std::endl;
+	std::cin >> t_meleeOrSpell;
+
+	if (t_meleeOrSpell == 1)
+	{
+		if (t_orc.getMeleeNum() > 0)
+		{
+			t_troll.meleeDamage();
+			t_troll.meleeDamageOutput();
+			t_orc.meleeUsed();
+		}
+		else
+		{
+			std::cout << t_orc.getName() + " does not have enough strength left for a melee attack and so he recieves a melee attack." << std::endl;
+			t_orc.meleeDamage();
+			t_orc.meleeDamageOutput();
+		}
+	}
+
+	if (t_meleeOrSpell == 2)
+	{
+		if (t_orc.getSpellsNum() > 0)
+		{
+			t_troll.spellDamage();
+			t_troll.spellDamageOutput();
+			t_orc.spellUsed();
+		}
+		else
+		{
+			std::cout << t_orc.getName() + " does not have enough magic left for a spell attack and so he recieves a spell attack." << std::endl;
+			t_orc.spellDamage();
+		}
+	}
+}
+
+
+// unfinished
+void gettingAttacked(Orc t_orc, Troll t_troll, int t_meleeOrSpell)
+{
+	std::cout << t_troll.getName() + " is attacking " + t_orc.getName() + "! \nShould he 1: Block using shield or 2: Dodge ?" << std::endl;
+	std::cin >> t_meleeOrSpell;
+
+	if (t_meleeOrSpell == 1)
+	{
+		if (t_troll.getMeleeNum() > 0)
+		{
+			
+		}
+		else
+		{
+			std::cout << t_orc.getName() + " does not have enough strength left for a melee attack and so he recieves a melee attack." << std::endl;
+			t_orc.meleeDamage();
+			t_orc.meleeDamageOutput();
+		}
+	}
+
+	if (t_meleeOrSpell == 2)
+	{
+		if (t_orc.getSpellsNum() > 0)
+		{
+			t_troll.spellDamage();
+			t_troll.spellDamageOutput();
+			t_orc.spellUsed();
+		}
+		else
+		{
+			std::cout << t_orc.getName() + " does not have enough magic left for a spell attack and so he recieves a spell attack." << std::endl;
+			t_orc.spellDamage();
+		}
+	}
 }
